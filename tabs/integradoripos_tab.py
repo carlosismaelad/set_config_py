bdcd26725bcbb30e88e27d309bed021bec03cd86
carlosismaelad <carlosismaelad@outlook.com>
2024-09-24 22:48:54 -0300

    refact: Removes input fields not used by the program | Enforcement action is being carried out in app_settings_handler

--- tabs/integradoripos_tab.py
+++ tabs/integradoripos_tab.py
@@ -5,8 +5,3 @@ from PySide6.QtWidgets import QLabel, QLineEdit
 class IntegradoriposTab(TabBase):
     def __init__(self):
         super().__init__()
-
-        self.integradoripos_disable_notifications_sounds = QLineEdit()
-        self.integradoripos_disable_notifications_sounds.setPlaceholderText("'true' ou 'false'")
-        self.layout.addWidget(QLabel("Desabilitar sons de notificações"))
-        self.layout.addWidget(self.integradoripos_disable_notifications_sounds)
